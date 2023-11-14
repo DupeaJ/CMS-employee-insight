a80efe288f524764525ba429f6c9be50ee6ac0f0
unknown <jacobdupea@gmail.com>
2023-11-13 21:42:16 -0800

    added .env variables for user

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: "root",
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: 'employeedb'
 });
